ceb9171d8d8eab20964e11dc77f528fe375bc4d4
arigelis <granistad@gmail.com>
2020-10-17 18:19:12 +0500

    first

--- src/test/java/ru/ruthen/resttemplate/RestTemplateApplicationTests.java
+++ src/test/java/ru/ruthen/resttemplate/RestTemplateApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestTemplateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
+//test
 }
